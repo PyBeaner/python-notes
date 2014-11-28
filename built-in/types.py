60569a00b82f93d9c710663e9a00176cb88c901c
PyBeaner <758046738@qq.com>
2014-11-28 23:35:41 +0800

    numeric types

--- built-in/types.py
+++ built-in/types.py
@@ -1,1 +1,17 @@
 __author__ = 'pybeaner'
+
+# false
+print any([False, None, 0, 0L, 0.0, 0j, '', (), [], {}])
+class C:
+    # def __len__(self):
+    #     return False
+    def __nonzero__(self):
+        return 0
+if not C():
+    print 'False'
+
+
+# Numeric Types
+import sys
+print sys.maxint
+print sys.float_info
