14460af2e0b045f998829fbdf27d838b880c5edb
PyBeaner <758046738@qq.com>
2014-11-30 10:40:23 +0800

    import string

---
+++ String Services/string_module.py
@@ -0,0 +1,11 @@
+__author__ = 'pybeaner'
+import string
+
+# string constants
+print string.ascii_lowercase, string.ascii_uppercase, string.ascii_letters
+print string.digits, string.hexdigits, string.octdigits
+print string.punctuation
+print repr(string.whitespace)
+print repr(string.printable) # a combination of digits, letters, punctuation, and whitespace
+
+# string formatting
